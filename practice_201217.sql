08915deaba1db882850ddc57efc2c9c80e998490
김진하 <jinhak94@naver.com>
2020-12-17 15:50:33 +0900

    select 실습문제 1

---
+++ practice_201217.sql
@@ -0,0 +1,10 @@
+desc employee;
+--1.
+select emp_name, salary*12, 
+       salary + (salary * nvl(bonus,0)) as total_salary,
+        salary + (salary * nvl(bonus,0))-(salary * 0.03) as real_salary 
+from employee;
+
+--2. 
+select emp_name, hire_date, round(sysdate-hire_date)
+from employee;
